4140ecec11e0921bc6fb06dcc4882d3613cd18b3
yenlinh000 <yenlinh.nc21@gmail.com>
2021-04-09 16:01:11 +0700

    Add files via upload

---
+++ admin-main/connect.php
@@ -0,0 +1,4 @@
+<?php
+$conn = pg_connect("host=ec2-34-225-103-117.compute-1.amazonaws.com port=5432 dbname=dcg72tr1l83dd3 user=eophfavabkrorx password=cada536917ba323da06136f1b0fd36d8eeae9fee194a8d611e249fd04c60fa78");
+if(!$conn){echo "Lost Connect";}
+?>
